7b17d4146fe64d1a4a57489e2993e24dd0d229a7
KiloWattDot <74320819+KiloWattDot@users.noreply.github.com>
2021-12-26 21:15:20 -0500

    made notes, added prompt questions

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require('util');
+
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+}, { 
+    type: "input",
+    message: "What is a description of this project?",
+    name: "Descrption"
+}, {
+    type: "confirm", 
+    message: "Will you be including an table of contents? (Y/N)",
+    name: "Table of Contents"
+}, {
+    type: "input",
+    message: "What is the installation requirements for this app?",
+    name: "Installation"
+}, {
+    type: "input",
+    message: "How will this app be used?",
+    name: "Usage"
+}, {
+    type: "input",
+    message: "What is the license being used?",
+    name: "License"
+}, {
+    type: "input",
+    message: "Who are the contributors of this project?",
+    name: "Contributing"
+}, {
+    type:"input",
+    message: "What are the commands needed to test this app?",
+    name: "Tests"
+}, {
+    type: "input",
+    message: "Where can you be contacted?",
+    name: "Questions"
+}, {
+    type: "input",
+    message: "What is your Github username",
+    name: "Username"
+}, {
+    type: "input",
+    message: "What is your email address?",
+    name: "Email"
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
