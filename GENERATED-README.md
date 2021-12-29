921e3f6bfee7fc140191ccf00674bf21bc2116de
KiloWattDot <74320819+KiloWattDot@users.noreply.github.com>
2021-12-29 11:19:01 -0500

    added license code, generateMarkdown, npm i inquirer, fixed prompt questions. code works for the most part

---
+++ GENERATED-README.md
@@ -0,0 +1,55 @@
+# Weather App
+
+   ## License
+      [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  ## Description
+  Weather Dashboard
+  
+  ## Table of Contents
+  -[ Title](#title)
+
+  -[ Description](#description)
+
+  -[ Installation](#installation)
+
+  -[ Usage](#usage)
+
+  -[ License](#license)
+
+  -[ Contributing](#contributing)
+
+  -[ Tests](#tests)
+
+  -[ Questions](#questions)
+
+  ## Installation
+  console.log
+
+  ## Usage
+  * Do not forget to add a screenshot of your final app.
+  * First, create an assets/images folder in your repository and upload your screenshot to it.
+  * Then, using the relative filepath, add it to your README using the following syntax:
+  * !&lsqb;alt text&lsqb;(assets/images/screenshot.png)
+  
+  ## License
+  MIT
+
+  ## Contributing
+  Anything
+
+  ## Tests
+  npm run
+  
+  ## Questions
+
+  github.com/kilowattdot
+
+  doro.g@gmail.com
+
+
+
+
+
+
+   
+
--- index.js
+++ index.js
@@ -2,69 +2,87 @@
 
 console.log("Program Has started.")
 
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
-
 const generateMarkdown = require('./utils/generateMarkdown');
 
+
 // TODO: Create an array of questions for user input
-const questions = [{
-    type: "input",
-    message: "What is the title of the project?",
-    name: "Title"
-}, { 
-    type: "input",
-    message: "What is a description of this project?",
-    name: "Descrption"
-}, {
-    type: "confirm", 
-    message: "Will you be including an table of contents? (Y/N)",
-    name: "Table of Contents"
-}, {
-    type: "input",
-    message: "What is the installation requirements for this app?",
-    name: "Installation"
-}, {
-    type: "input",
-    message: "How will this app be used?",
-    name: "Usage"
-}, {
-    type: "input",
-    message: "What is the license being used?",
-    name: "License"
-}, {
-    type: "input",
-    message: "Who are the contributors of this project?",
-    name: "Contributing"
-}, {
-    type:"input",
-    message: "What are the commands needed to test this app?",
-    name: "Tests"
-}, {
-    type: "input",
-    message: "Where can you be contacted?",
-    name: "Questions"
-}, {
-    type: "input",
-    message: "What is your Github username",
-    name: "Username"
-}, {
-    type: "input",
-    message: "What is your email address?",
-    name: "Email"
-}];
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project name?",
+    },{ 
+      type: "input",
+      name: "description",
+      message:
+        "Give a brief description of your project. Why did you build it? What problem does it solve?",
+    },
+    //Table of Contents here for linking through the GENERATED-README.md file
+    {
+      type: "input",
+      name: "installation",
+      message: "What commands should we run for dependencies?",
+      default: "npm i",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What kind of license does your project use?",
+      choices: ["MIT", "Apache 2.0", "IBM-IPL 1.0", "None"],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "How does a user contribute to this project?",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "What commands should be run for tests?",
+      default: "npm run test",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "GitHub",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "Email",
+    },
+  ];
+  
+  
+
+  
+  
+  
+  
+  
+
+  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 
+    fs.writeFile("GENERATED-README.md", data, (err) => {
+        err
+          ? console.log(err)
+          : console.log("GENERATED-README.md file was created and written!");
+      });
 
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions).then((data) => {
+        writeToFile("GENERATED-README.md", generateMarkdown(data));
+      });
+}
 
 // Function call to initialize app
 init();
 
-console.log("Program Has ended.")
